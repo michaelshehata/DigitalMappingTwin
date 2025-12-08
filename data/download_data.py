49f720ef603c9df47f2e46df2b6cf0f44950c2c3
Michael Shehata <michaelfireal@gmail.com>
2025-12-08 12:47:53 +0000

    chore: Fixes to download script and extra configs

--- data/download_data.py
+++ data/download_data.py
@@ -4,7 +4,7 @@ import os
 kaggle.api.authenticate()
 
 kaggle.api.dataset_download_files(
-    'sudalairajkumar/daily-temperature-of-major-citiess',
+    'sudalairajkumar/daily-temperature-of-major-cities',
     path='raw/data', 
     unzip=True
 )
